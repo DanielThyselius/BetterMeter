2af0126238f24543aa597b53ff046c5fbc562eb3
Daniel Thyselius <daniel@mindfulstack.se>
2025-03-24 10:37:06 +0100

    wip: adding blazor auth

--- BetterMeter.Client/Program.cs
+++ BetterMeter.Client/Program.cs
@@ -1,11 +1,35 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BetterMeter.Client;
+using BetterMeter.Identity;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// register the cookie handler
+builder.Services.AddTransient<CookieHandler>();
+
+// set up authorization
+builder.Services.AddAuthorizationCore();
+
+// register the custom state provider
+builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
+
+// register the account management interface
+builder.Services.AddScoped(
+    sp => (IAccountManagement)sp.GetRequiredService<AuthenticationStateProvider>());
+
+// set base address for default host
+// Where the blazor wasm is hosted (right now, our devServer)
+builder.Services.AddScoped(sp =>
+    new HttpClient { BaseAddress = new Uri(builder.Configuration["FrontendUrl"] ?? "https://localhost:7102/") });
+
+// configure client for auth interactions
+builder.Services.AddHttpClient(
+    "Auth",
+    opt => opt.BaseAddress = new Uri(builder.Configuration["BackendUrl"] ?? "https://localhost:7101"))
+    .AddHttpMessageHandler<CookieHandler>();
 
 await builder.Build().RunAsync();
